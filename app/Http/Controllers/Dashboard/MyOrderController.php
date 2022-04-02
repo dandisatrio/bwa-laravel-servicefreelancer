de3d0c833034fbfc94c4523df52562ac18cea7e4
dandisatrio <dandisatrio810@gmail.com>
2022-04-02 22:40:47 +0700

    Development Backend

--- app/Http/Controllers/Dashboard/MyOrderController.php
+++ app/Http/Controllers/Dashboard/MyOrderController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\MyOrder\UpdateMyOrderRequest;
+use App\Models\AdvantageService;
+use App\Models\AdvantageUser;
+use App\Models\Order;
+use App\Models\Service;
+use App\Models\Tagline;
+use App\Models\ThumbnailService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MyOrderController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +27,9 @@ class MyOrderController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.order.index');
+        $orders = Order::where('freelancer_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+
+        return view('pages.dashboard.order.index', compact('orders'));
     }
 
     /**
@@ -24,7 +39,7 @@ class MyOrderController extends Controller
      */
     public function create()
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -35,7 +50,7 @@ class MyOrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -44,9 +59,17 @@ class MyOrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Order $order)
     {
-        return view('pages.dashboard.order.detail');
+        $service = Service::where('id', $order['service_id'])->first();
+        $thumbnail = ThumbnailService::where('service_id', $order['service_id'])->get();
+        $advantage_service = AdvantageService::where('service_id', $order['service_id'])->get();
+        $advantage_user = AdvantageUser::where('service_id', $order['service_id'])->get();
+        $tagline = Tagline::where('service_id', $order['service_id'])->get();
+
+        return view('pages.dashboard.order.detail', compact(
+            'order', 'service', 'thumbnail', 'advantage_service', 'advantage_user', 'tagline'
+        ));
     }
 
     /**
@@ -57,7 +80,7 @@ class MyOrderController extends Controller
      */
     public function edit($id)
     {
-        return view('pages.dashboard.order.edit');
+        return view('pages.dashboard.order.edit', compact('order'));
     }
 
     /**
@@ -67,9 +90,23 @@ class MyOrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateMyOrderRequest $request, Order $order)
     {
-        //
+        $data = $request->all();
+
+        if(isset($data['file'])) {
+            $data['file'] = $request->file('file')->store(
+                'assets/order/attachment', 'public'
+            );
+        }
+        
+        $order = Order::find($order->id);
+        $order->file = $data['file'];
+        $order->note = $data['note'];
+        $order->save();
+
+        toast()->success('Submit order has been success');
+        return redirect()->route('member.order.index');
     }
 
     /**
@@ -80,16 +117,26 @@ class MyOrderController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return abort(404);
     }
 
     public function accepted($id)
     {
-        //
+        $order = Order::find($id);
+        $order->order_status_id = 2;
+        $order->save();
+
+        toast()->succes('Accept order has been success');
+        return back();
     }
 
     public function rejected($id)
     {
-        //
+        $order = Order::find($id);
+        $order->order_status_id = 3;
+        $order->save();
+
+        toast()->succes('Reject order has been success');
+        return back();
     }
 }
--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+
+use App\Http\Requests\Dashboard\Profile\UpdateProfileRequest;
+use App\Http\Requests\Dashboard\Profile\UpdateDetailUserRequest;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+
+use App\Models\DetailUser;
+use App\Models\ExperienceUser;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +33,12 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard.profile');
+        $user = User::where('id', Auth::user()->id)->first();
+        $experience_user = ExperienceUser::where('detail_user_id', $user->detail_user->id)
+                                            ->orderBy('id', 'asc')
+                                            ->get();
+
+        return view('pages.dashboard.profile', compact('user', 'experience_user'));
     }
 
     /**
@@ -24,7 +48,7 @@ class ProfileController extends Controller
      */
     public function create()
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -46,7 +70,7 @@ class ProfileController extends Controller
      */
     public function show($id)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -57,7 +81,7 @@ class ProfileController extends Controller
      */
     public function edit($id)
     {
-        //
+        return abort(404);
     }
 
     /**
@@ -67,9 +91,65 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateProfileRequest $request_profile, UpdateDetailUserRequest $request_detail_user)
     {
-        //
+        $data_profile = $request_profile->all();
+        $data_detail_user = $request_detail_user->all();
+
+        // get photo user
+        $get_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+
+        // delete old file
+        if(isset($data_detail_user['photo'])){
+            $data = 'storage/'.$get_photo['photo'];
+            if(File::exists($data)){
+                File::delete($data);
+            } else {
+                File::delete('storage/app/public/'.$get_photo['photo']);
+            }
+        }
+
+        // store file to storage
+        if(isset($data_detail_user['photo'])){
+            $data_detail_user['photo'] = $request_detail_user->file('photo')->store(
+                'assets/photo', 'public'
+            );
+        }
+
+        // process save to user
+        $user = User::find(Auth::user()->id);
+        $user->update($data_profile);
+
+        // process save to detail user
+        $detail_user = DetailUser::find($user->detail_user->id);
+        $detail_user->update($data_detail_user);
+
+        // process save to experience
+        $experience_user_id = DetailUser::where('detail_user_id', $detail_user['id'])->first();
+        if(isset($experience_user_id)) {
+
+            foreach ($data_profile['experience'] as $key => $value) {
+                $experience_user = ExperienceUser::find($key);
+                $experience_user->detail_user_id = $detail_user['id'];
+                $experience_user->experience = $value;
+                $experience_user->save();
+            }
+
+        } else {
+            foreach ($data_profile['experience'] as $key => $value) {
+                if(isset($value)) {
+                    $experience_user = new ExperienceUser();
+                    $experience_user->detail_user_id = $detail_user['id'];
+                    $experience_user->experience = $value;
+                    $experience_user->save();
+
+                }
+
+            }
+        }
+
+        toast()->success('Update has been success');
+        return back();
     }
 
     /**
@@ -80,11 +160,29 @@ class ProfileController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return abort(404);
     }
 
     public function delete()
     {
-        //
+        // get user
+        $get_user_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+        $path_photo = $get_user_photo['photo'];
+
+        // second update value to null
+        $data = DetailUser::find($get_user_photo['id']);
+        $data->photo = NULL;
+        $data->save();
+
+        // delete file photo
+        $data = 'storage/'.$path_photo;
+        if(File::exists($data)) {
+            File::delete($data);
+        } else {
+            File::delete('storage/app/public/'.$path_photo);
+        }
+
+        toast()->success('Delete has been success');
+        return back();
     }
 }
